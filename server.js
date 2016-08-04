d888bd22d613f680f89d505ff5f51d011ae7c7b2
Claire Mitchell <claireinezmitchell@gmail.com>
2016-08-04 21:35:27 +0100

    Move fs.readFile within the handler function.

--- server.js
+++ server.js
@@ -4,42 +4,46 @@ var fs = require('fs');
 // set the port for the server
 var port = process.env.PORT || 8000;
 
-fs.readFile(__dirname + '/index.html', function (error, index) {
 
-  function handler(request, response) {
-    var url = request.url;
+function handler(request, response) {
 
-    if (url.length === 1) {
+  var url = request.url;
+
+  if (url.length === 1) {
+
+    fs.readFile(__dirname + '/index.html', function (error, index) {
 
       response.writeHead(200, {"Content-Type": "text/html"});
       response.end(index);
 
-    } else if (url.indexOf('/cat') > -1) {
+    });
 
-      response.writeHead(200, {"Content-Type": "text/html"});
-      response.end('<img src="http://charts.stocktwits.com/production/original_24310845.jpg?1404265667"/>');
+  } else if (url.indexOf('/cat') > -1) {
 
-    } else {
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.end('<img src="http://charts.stocktwits.com/production/original_24310845.jpg?1404265667"/>');
 
-      fs.readFile(__dirname + url, function(error, file){
+  } else {
 
-        if (error){
+    fs.readFile(__dirname + url, function(error, file){
 
-          console.log(error);
-          response.end();
+      if (error){
 
-        } else {
+        console.log(error);
+        response.end();
 
-          var ext = url.split('.')[1];
-          response.writeHead(200, {'Content-Type' : 'text/' + ext});
-          response.end(file);
+      } else {
 
-        }
-      });
-    }
+        var ext = url.split('.')[1];
+        response.writeHead(200, {'Content-Type' : 'text/' + ext});
+        response.end(file);
+
+      }
+    });
   }
+}
 
-  http.createServer(handler).listen(port);
-});
+http.createServer(handler).listen(port);
+
 
 console.log('node http server listening on http://localhost:' + port);
